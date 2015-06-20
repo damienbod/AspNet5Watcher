5568800922c9786d305d844f5f0d531fd3106e10
damienbod <damien_bod@hotmail.com>
2015-06-20 21:28:13 +0200

    code clean up

--- src/AspNet5Watcher/SearchEngine/SearchRepository.cs
+++ src/AspNet5Watcher/SearchEngine/SearchRepository.cs
@@ -9,23 +9,25 @@ namespace AspNet5Watcher.SearchEngine
     public class SearchRepository
     {
         private ElasticClient client;
+        private const string INDEX_ALARMMESSAGE = "alarms";
+        private const string TYPE_ALARMMESSAGE = "alarm";
 
         public SearchRepository(IConfiguration configuration)
         {           
             var node = new Uri(configuration.Get("Development:ElasticsearchConnectionString"));
 
             var settings = new ConnectionSettings( node,  defaultIndex: "coolsearchengine");
-            settings.MapDefaultTypeIndices(d => d.Add(typeof(AlarmMessage), "alarms"));
-            settings.MapDefaultTypeNames(d => d.Add(typeof(AlarmMessage), "alarm"));
+            settings.MapDefaultTypeIndices(d => d.Add(typeof(AlarmMessage), INDEX_ALARMMESSAGE));
+            settings.MapDefaultTypeNames(d => d.Add(typeof(AlarmMessage), TYPE_ALARMMESSAGE));
 
             client = new ElasticClient(settings);
         }
 
         public void AddDocument(AlarmMessage alarm)
         {
-            client.Index<AlarmMessage>(alarm, i => i
-                .Index("alarms")
-                .Type("alarm")
+            client.Index(alarm, i => i
+                .Index(INDEX_ALARMMESSAGE)
+                .Type(TYPE_ALARMMESSAGE)
                 .Refresh()
             );
         }
