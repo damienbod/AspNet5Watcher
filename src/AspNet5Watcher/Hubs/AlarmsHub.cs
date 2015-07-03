63c43a85b8eb9a50d5281dd853ee373dca09c64b
damienbod <damien_bod@hotmail.com>
2015-07-04 00:14:26 +0200

    code clean up

--- src/AspNet5Watcher/Hubs/AlarmsHub.cs
+++ src/AspNet5Watcher/Hubs/AlarmsHub.cs
@@ -1,28 +1,11 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
-
 namespace AspNet5Watcher.Hubs
 {
     [HubName("alarms")]
     public class AlarmsHub  : Hub
     {
-        
-
-        //public override Task OnConnected()
-        //{
-        //    return (base.OnConnected());
-        //}
-
-        //public override Task OnDisconnected(bool stopCalled)
-        //{
-        //    return (base.OnDisconnected(stopCalled));
-        //}
-
-        //public override Task OnReconnected()
-        //{
-        //    return (base.OnReconnected());
-        //}
     }
 
 }
